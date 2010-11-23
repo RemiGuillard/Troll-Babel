e8b62ea34e67482afbf0313a86f6884bf02168ed
laurent lolo <lolokun@gmail.com>
2010-11-23 15:27:56 +0100

    Ajout IOStreamData.hpp

---
+++ Babel/IOStreamData.hpp
@@ -0,0 +1,21 @@
+#ifndef		IOSTREAMDATA
+# define	IOSTREAMDATA
+
+#define FRAMES_PER_BUFFER (160)
+
+template <typename T>
+struct IOStreamData
+{
+	T		IBuf[FRAMES_PER_BUFFER];
+	bool	IAvailable; // a transformer en mutex
+	int		IFrameIndex;  /* Index into sample array. */
+    int		IMaxFrameIndex;
+	T		OBuf[FRAMES_PER_BUFFER];
+	bool	OAvailable; // mutex
+	int		OFrameIndex;  /* Index into sample array. */
+    int		OMaxFrameIndex;
+	char	encoded[FRAMES_PER_BUFFER];
+	int		ThreadEnd;
+};
+
+#endif	//	!IOSTREAMDATA
